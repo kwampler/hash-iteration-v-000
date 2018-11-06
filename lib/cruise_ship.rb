de16bf822f5c5a55279174d562c7fec1ef7db680
katejsenger <katejsenger@yahoo.com>
2018-11-06 19:15:23 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -14,6 +14,7 @@ passengers.each do |suite, name|
 end
 end
 end
+winner
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
